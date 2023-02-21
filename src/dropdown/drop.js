ddd4f999681fa62b281e8da09fe010d1b087bf6f
milleniya21 <milleniyasajy21@gmail.com>
2023-02-21 14:17:17 +0530

    dropdown_styling_done

--- src/dropdown/drop.js
+++ src/dropdown/drop.js
@@ -6,24 +6,24 @@ function Drop() {
     <div className='section'>
         <form action="/action_page.php">  
             <p>Supplier</p>                    
-            <select  id="sort">
+            <select  id="sort1">
                 <option value="Supplier">Select a supplier  </option>
             </select>      
-            <button id =" btn">+ New</button>                   
+            <button> + New </button>                   
         </form>  
         <form action="/action_page.php"> 
             <p>Product type</p>                     
-            <select  id="sort">
+            <select  id="sort2">
                 <option value="Product type ">Select a type  </option>
             </select>       
-            <button id =" btn">+ New</button>                  
+            <button id =" btn2">+ New</button>                  
         </form>  
         <form action="/action_page.php">   
             <p>Brand</p>                   
-            <select id="sort">
+            <select id="sort3">
                 <option value="Brand">Select a supplier</option>
             </select>                         
-            <button id =" btn">+ New</button>
+            <button id =" btn3">+ New</button>
         </form>  
       
       
